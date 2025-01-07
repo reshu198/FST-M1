84ca620136bfba98513e3c35b98345d94d11e6f4
reshu198 <rashmi.singh19@in.ibm.com>
2025-01-07 12:31:29 +0530

    Add files via upload

---
+++ Python/Activities/Activity2.py
@@ -0,0 +1,8 @@
+# Activity 2- Odd and Even number 
+
+inputNum= int(input("Enter a number: "))
+
+if (inputNum % 2) ==0:
+    print(str(inputNum)+ " is a even number")
+else:
+    print(str(inputNum) + " is odd number ")
