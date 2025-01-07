0cf0f6e4916ee821bd2b0554bb62e3f96a411bf3
reshu198 <rashmi.singh19@in.ibm.com>
2025-01-07 23:45:49 +0530

    Add files via upload

--- Python/Activities/Activity19.py
+++ Python/Activities/Activity19.py
@@ -6,9 +6,9 @@ from pandas import ExcelWriter
 
 # Create a dictionary that will be used to create the DataFrame
 data = {
-	'FirstName':["Shekhar", "Avinash", "Tejas"],
+	'FirstName':["Satvik", "Avinash", "Lahri"],
 	'LastName':["Shah", "Kati", "Rath"],
-	'Email':["shekhar@example.com", "avinashK@example.com", "tejas.rath@example.com"],
+	'Email':["satshah@example.com", "avinashK@example.com", "lahri.rath@example.com"],
 	'PhoneNumber':["4537829158", "4892184058", "4528727830"]
 }
 
